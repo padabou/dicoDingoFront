e2ef128adeb330ea8d1af4ba17e1a35d06329e6e
de l'Hamaide <delhamaide.florent@gmail.com>
2024-09-05 23:51:42 +0200

    fix: build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  productionBrowserSourceMaps: false,
   reactStrictMode: true,
   swcMinify: true,
   images: {
