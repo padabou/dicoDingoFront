fe6ab2980cbd8a7727ea43b34d8bf1fa28496f4c
de l'Hamaide <delhamaide.florent@gmail.com>
2024-09-06 15:46:35 +0200

    fix: build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import "@/styles/tailwind.css";
 import { Providers } from "./providers";
 import { cx } from "@/utils/all";
 import { Inter, Lora } from "next/font/google";
-import React, {Suspense} from "react";
-import Loading from "@/components/loading";
+import React from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,9 +25,7 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
       <body className="antialiased text-gray-800 dark:bg-black dark:text-gray-400">
-      <Suspense fallback={<Loading />}>
         <Providers>{children}</Providers>
-      </Suspense>
       </body>
     </html>
   );
