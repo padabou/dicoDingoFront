b7c97badfaba09a1d4206ac73c1a031f64ec9e54
de l'Hamaide <delhamaide.florent@gmail.com>
2024-10-04 14:40:42 +0200

    feat: update couleur to robe

--- components/navbar.js
+++ components/navbar.js
@@ -17,8 +17,8 @@ export default function Navbar(props) {
       href: "/"
     },
     {
-      label: "Couleur",
-      href: "/article/couleur"
+      label: "Robe",
+      href: "/article/robe"
     },
     {
       label: "Discipline",
