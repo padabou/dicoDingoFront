fa13874aa2bd27ed54cc93d444e3bc8c5559dd8b
de l'Hamaide <delhamaide.florent@gmail.com>
2024-10-16 23:34:21 +0200

    feat: remove useless header

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  poweredByHeader: false,
   productionBrowserSourceMaps: false,
   reactStrictMode: true,
   swcMinify: true,
