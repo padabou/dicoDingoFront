e02efe188f049e40eea5dee98a2289f0297c827c
de l'Hamaide <delhamaide.florent@gmail.com>
2025-10-21 14:53:30 +0200

    feat: add contact page to footer

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer(props) {
   return (
     <Container className="mt-10 border-t border-gray-100 dark:border-gray-800">
       <div className="text-center text-sm">
-        Copyright © {new Date().getFullYear()} {props?.copyright}. All rights reserved. <Link className={"aspect-square"} href={`/mentions-legales`}>Mentions légales</Link> - <Link className={"aspect-square"} href={`/politique-de-confidentialite`}>Politique de confidentialité</Link> - <Link className={"aspect-square"} href={`/about`}>A propos de nous</Link>
+        Copyright © {new Date().getFullYear()} {props?.copyright}. All rights reserved. <Link className={"aspect-square"} href={`/mentions-legales`}>Mentions légales</Link> - <Link className={"aspect-square"} href={`/politique-de-confidentialite`}>Politique de confidentialité</Link> - <Link className={"aspect-square"} href={`/about`}>A propos de nous</Link> - <Link className={"aspect-square"} href={`/contact`}>Contactez-nous</Link>
       </div>
       <div className="mt-1 flex justify-center gap-1 text-center text-sm text-gray-500 dark:text-gray-600">
         <span>
